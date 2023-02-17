93dc4bcae5648c726bc31780c7e32c45e93132e9
Per Bly <per.bly@aw.com>
2023-02-17 08:50:13 +0100

    helix name

--- modules/onify-customer/onify-application-helix.tf
+++ modules/onify-customer/onify-application-helix.tf
@@ -1,6 +1,6 @@
-resource "kubernetes_config_map" "onify-app-v2" {
+resource "kubernetes_config_map" "onify-app-helix" {
   metadata {
-    name      = "${local.client_code}-${local.onify_instance}-app-v2"
+    name      = "${local.client_code}-${local.onify_instance}-app-helix"
     namespace = "${local.client_code}-${local.onify_instance}"
   }
 
@@ -10,29 +10,29 @@ resource "kubernetes_config_map" "onify-app-v2" {
   depends_on = [kubernetes_namespace.customer_namespace]
 }
 
-resource "kubernetes_stateful_set" "onify-app-v2" {
+resource "kubernetes_stateful_set" "onify-app-helix" {
   metadata {
-    name      = "${local.client_code}-${local.onify_instance}-app-v2"
+    name      = "${local.client_code}-${local.onify_instance}-app-helix"
     namespace = kubernetes_namespace.customer_namespace.metadata.0.name
     labels = {
-      app  = "${local.client_code}-${local.onify_instance}-app-v2"
-      name = "${local.client_code}-${local.onify_instance}-app-v2"
+      app  = "${local.client_code}-${local.onify_instance}-app-helix"
+      name = "${local.client_code}-${local.onify_instance}-app-helix"
     }
   }
   spec {
-    service_name = "${local.client_code}-${local.onify_instance}-app-v2"
+    service_name = "${local.client_code}-${local.onify_instance}-app-helix"
     replicas     = var.deployment_replicas
     selector {
       match_labels = {
-        app  = "${local.client_code}-${local.onify_instance}-app-v2"
-        task = "${local.client_code}-${local.onify_instance}-app-v2"
+        app  = "${local.client_code}-${local.onify_instance}-app-helix"
+        task = "${local.client_code}-${local.onify_instance}-app-helix"
       }
     }
     template {
       metadata {
         labels = {
-          app  = "${local.client_code}-${local.onify_instance}-app-v2"
-          task = "${local.client_code}-${local.onify_instance}-app-v2"
+          app  = "${local.client_code}-${local.onify_instance}-app-helix"
+          task = "${local.client_code}-${local.onify_instance}-app-helix"
         }
       }
       spec {
@@ -43,7 +43,7 @@ resource "kubernetes_stateful_set" "onify-app-v2" {
           image = "eu.gcr.io/onify-images/hub/app:${var.onify-app_version}"
           name  = "onfiy-app"
           port {
-            name           = "onify-app-v2"
+            name           = "onify-app-helix"
             container_port = 3000
           }
           dynamic "env" {
@@ -55,7 +55,7 @@ resource "kubernetes_stateful_set" "onify-app-v2" {
           }
           env_from {
             config_map_ref {
-              name = "${local.client_code}-${local.onify_instance}-app-v2"
+              name = "${local.client_code}-${local.onify_instance}-app-helix"
             }
           }
 
@@ -66,9 +66,9 @@ resource "kubernetes_stateful_set" "onify-app-v2" {
   depends_on = [kubernetes_namespace.customer_namespace]
 }
 
-resource "kubernetes_service" "onify-app-v2" {
+resource "kubernetes_service" "onify-app-helix" {
   metadata {
-    name      = "${local.client_code}-${local.onify_instance}-app-v2"
+    name      = "${local.client_code}-${local.onify_instance}-app-helix"
     namespace = kubernetes_namespace.customer_namespace.metadata.0.name
     annotations = {
       "cloud.google.com/load-balancer-type" = "Internal"
@@ -76,11 +76,11 @@ resource "kubernetes_service" "onify-app-v2" {
   }
   spec {
     selector = {
-      app  = "${local.client_code}-${local.onify_instance}-app-v2"
-      task = "${local.client_code}-${local.onify_instance}-app-v2"
+      app  = "${local.client_code}-${local.onify_instance}-app-helix"
+      task = "${local.client_code}-${local.onify_instance}-app-helix"
     }
     port {
-      name     = "onify-app-v2"
+      name     = "onify-app-helix"
       port     = 3000
       protocol = "TCP"
     }
@@ -89,10 +89,10 @@ resource "kubernetes_service" "onify-app-v2" {
   depends_on = [kubernetes_namespace.customer_namespace]
 }
 
-resource "kubernetes_ingress_v1" "onify-app-v2" {
+resource "kubernetes_ingress_v1" "onify-app-helix" {
   wait_for_load_balancer = false
   metadata {
-    name      = "${local.client_code}-${local.onify_instance}-app-v2"
+    name      = "${local.client_code}-${local.onify_instance}-app-helix"
     namespace = kubernetes_namespace.customer_namespace.metadata.0.name
     annotations = {
       "cert-manager.io/cluster-issuer" = "letsencrypt-${var.tls}"
@@ -100,24 +100,24 @@ resource "kubernetes_ingress_v1" "onify-app-v2" {
   }
   spec {
     tls {
-      hosts = ["${local.client_code}-${local.onify_instance}-app-v2.${var.external-dns-domain}"]
-      secret_name = "tls-secret-app-v2-${var.tls}"
+      hosts = ["${local.client_code}-${local.onify_instance}-app-helix.${var.external-dns-domain}"]
+      secret_name = "tls-secret-app-helix-${var.tls}"
     }
     dynamic "tls" {
       for_each = var.custom_hostname!= null ? [1] : []
       content {
         hosts = ["${var.custom_hostname}.${var.external-dns-domain}"]
-        secret_name = "tls-secret-app-v2-${var.tls}-custom"
+        secret_name = "tls-secret-app-helix-${var.tls}-custom"
       }
     }
     ingress_class_name = "nginx"
     rule {
-    host = "${local.client_code}-${local.onify_instance}-app-v2.${var.external-dns-domain}"
+    host = "${local.client_code}-${local.onify_instance}-app-helix.${var.external-dns-domain}"
       http {
         path {
           backend {
             service {
-              name = "${local.client_code}-${local.onify_instance}-app-v2"
+              name = "${local.client_code}-${local.onify_instance}-app-helix"
             port {
               number = 3000
             }
@@ -134,7 +134,7 @@ resource "kubernetes_ingress_v1" "onify-app-v2" {
           path {
           backend {
             service {
-              name = "${local.client_code}-${local.onify_instance}-app-v2"
+              name = "${local.client_code}-${local.onify_instance}-app-helix"
             port {
               number = 3000
                 }
